c89d64c60ee5da187c52f13b0114e464795214b5
AfanasevAlexey <ya.alexey.27@mail.ru>
2019-02-27 23:23:26 +0900

    С первого урока записал коды

---
+++ LessonOne/main.swift
@@ -0,0 +1,53 @@
+import Foundation
+
+//var valueFirst : Double = 5.003
+//var valueSecond : Float = 5.003
+//
+//let costant : Float = Float (valueFirst) + valueSecond
+//print(costant)
+
+
+
+//var array2 = Array (repeating: 100, count: 50)
+//
+//print (array2)
+// здесь я создал так что бы значение 100 повторялось 50 раз
+
+
+
+//var array : [String] = ["1","2","50"]
+//print (array)
+// здесь числа отображаются в строку
+
+
+//var array : [Int : String] = [Int : String] ()
+//array [0] = "value"
+//array [100] = "value100"
+//array [0] = "faca"
+//print(array)
+//// здесь ключ соответствует значению
+
+//var array : [String : String] = [String : String] ()
+//
+//array ["1234 123456"] = "Alexandr Petrov"
+//array ["4321 654321"] = "Anna Sidorova"
+//array ["1234 123456"] = "Alexandr Ivanov"
+//print (array)
+//// здесь я заменил в первом ключе фамилию Алесандра на ivanov, система прочитала исправленный код.
+//
+//var set = Set<Character>()
+//var value : Set<String> = ["one","two","three"]
+//value.insert("four")
+//print(value.count)
+//// просто валу это показывает все значения, .count показывает количество
+
+var x = 2
+var y = 0
+
+if x > y {
+ print("okey")
+} else {
+    print("false")
+}
+
+
